732fd1eee2acd4bd3ee4f12f6dfa061c0e708515
Pedro Guilherme Antico <passoka7@hotmail.com>
2026-04-09 16:03:56 -0300

    Update hac_final.py

--- hac_final.py
+++ hac_final.py
@@ -95,9 +95,9 @@ class HACPhysicsConfig:
     KP_SCALE = 9.0
     
     # LIMITES FÍSICOS
-    VSW_MIN, VSW_MAX = 200, 1500 # km/s200, 1500      # km/s
+    VSW_MIN, VSW_MAX = 200, 1500 # km/s
     DENSITY_MIN, DENSITY_MAX = 0.1, 100 # cm⁻³0.1, 100  # cm⁻³
-    BZ_MIN, BZ_MAX = -100, 100 # nT100, 100        # nT
+    BZ_MIN, BZ_MAX = -100, 100 # nT
     
     # NOWCAST + PARÂMETROS DE INÉRCIA# NOWCAST + INÉRCIA PARAMETERS
     THETA_CRITICAL = 50.0 # nT/h - Limiar de crescimento crítico50.0      # nT/h - Limiar de crescimento crítico
