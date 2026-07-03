26f8d7e27a18d717745a44fe5075d0fcf8971d72
Pedro Guilherme Antico <passoka7@hotmail.com>
2026-07-03 15:24:06 -0300

    Update hac_final.py

--- hac_final.py
+++ hac_final.py
@@ -49,7 +49,7 @@ class HACPhysicsConfig:
     # Parâmetros do modelo Burton (calibrados)
     VBs_THRESHOLD = 0.5
     Q_SCALE_CME = -320.0
-    Q_SCALE_HSS = -60.0
+    Q_SCALE_HSS = -45.0
     Q_SCALE_QUIET = -25.0
     Q_SATURATION = 12.0
     HAC_MODULATION_STRENGTH = 0.15
